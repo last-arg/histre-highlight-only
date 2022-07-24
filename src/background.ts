b615a41935d49e82a7f918d2961f1b1ceb93f5e5
last-arg <last_arg@tutanota.com>
2022-07-24 18:39:02 +0300

    Refactor Histre API code. Remove storage code from Histre API code.

--- src/background.ts
+++ src/background.ts
@@ -24,45 +24,6 @@ interface UserData {
   password: string,
 }
 
-async function authUser(user: UserData): Promise<AuthResp> {
-  const body = JSON.stringify(user);
-  const headers = { 
-    "Host": host,
-    "Content-Type": "application/json" 
-  };
-  const r = await fetch(authUrl, {
-    headers: headers,
-    method: 'POST',
-    body: body,
-  });
-  const auth_resp = await r.json();
-  // According to Histre API docs some responses might have 'status' field value as 'null'
-  if (!auth_resp.status) {
-    auth_resp.status = r.status;
-  }
-  return auth_resp as AuthResp
-};
-
-// Most endpoints return 403 (Forbidden) for auth errors.
-// Aquire and refresh endpoints return 401 for auth errors.
-async function refreshAuthToken(refresh: string): Promise<AuthResp> {
-  const headers = { 
-    "Host": host,
-    "Content-Type": "application/json" 
-  };
-  const r = await fetch(refreshAuthUrl, {
-    headers: headers,
-    method: 'POST',
-    body: `{"refresh": "${refresh}"}`,
-  });
-  const auth_resp = await r.json();
-  // According to Histre API docs some responses might have 'status' field value as 'null'
-  if (!auth_resp.status) {
-    auth_resp.status = r.status;
-  }
-  return auth_resp as AuthResp
-};
-
 interface AuthData {
   access: string,
   refresh: string,
@@ -79,10 +40,8 @@ async function getLocalAuthData(): Promise<AuthDataTime | undefined> {
   return undefined;
 }
 
-async function setLocalAuthData(token: AuthData): Promise<AuthDataTime> {
-  const result = {token: token, created_at: Date.now()};
-  await storage.local.set(result);
-  return result;
+async function setLocalAuthData(auth_data: AuthDataTime) {
+  await storage.local.set(auth_data);
 }
 
 async function getLocalUser(): Promise<UserData | undefined> {
@@ -97,111 +56,172 @@ async function setLocalUser(user: UserData): Promise<void> {
   await storage.local.set(user);
 }
 
-async function requestNewToken(curr_auth_data: AuthDataTime | undefined): Promise<AuthData | undefined> {
-  if (__DEV__) {; 
-    // Add test user data
-    const user = await import("../tmp/.secret.dev");
-    await setLocalUser(user.user)
-  }
+const histre = (function createHistre() {
+  async function authUser(user: UserData): Promise<AuthResp> {
+    const body = JSON.stringify(user);
+    const headers = { 
+      "Host": host,
+      "Content-Type": "application/json" 
+    };
+    const r = await fetch(authUrl, {
+      headers: headers,
+      method: 'POST',
+      body: body,
+    });
+    const auth_resp = await r.json();
+    // According to Histre API docs some responses might have 'status' field value as 'null'
+    if (!auth_resp.status) {
+      auth_resp.status = r.status;
+    }
+    return auth_resp as AuthResp
+  };
 
-  let auth_data = curr_auth_data?.token;
-  let err_msg: string | undefined = undefined;
-  if (auth_data) {
-    const now = new Date();
-    // NOTE: If we auth_data is valid then curr_auth_data is valid.
-    const created_date = new Date(curr_auth_data!.created_at);
-    const access_date = new Date(created_date);
-    access_date.setMinutes(access_date.getMinutes() + 15);
-    const refresh_date = new Date(created_date);
-    refresh_date.setDate(refresh_date.getDate() + 30);
+  // Most endpoints return 403 (Forbidden) for auth errors.
+  // Aquire and refresh endpoints return 401 for auth errors.
+  async function refreshAuthToken(refresh: string): Promise<AuthResp> {
+    const headers = { 
+      "Host": host,
+      "Content-Type": "application/json" 
+    };
+    const r = await fetch(refreshAuthUrl, {
+      headers: headers,
+      method: 'POST',
+      body: `{"refresh": "${refresh}"}`,
+    });
+    const auth_resp = await r.json();
+    // According to Histre API docs some responses might have 'status' field value as 'null'
+    if (!auth_resp.status) {
+      auth_resp.status = r.status;
+    }
+    return auth_resp as AuthResp
+  };
 
-    if (now > access_date) {
-      if (now < refresh_date) {
-        console.log("Refresh token", auth_data)
-        const resp = await refreshAuthToken(auth_data.refresh);
+  async function requestNewToken(curr_auth_data: AuthDataTime | undefined): Promise<AuthData | undefined> {
+    if (__DEV__) {; 
+      // Add test user data
+      const user = await import("../tmp/.secret.dev");
+      await setLocalUser(user.user)
+    }
+
+    let auth_data = curr_auth_data?.token;
+    let err_msg: string | undefined = undefined;
+    if (auth_data) {
+      const now = new Date();
+      // NOTE: If we auth_data is valid then curr_auth_data is valid.
+      const created_date = new Date(curr_auth_data!.created_at);
+      const access_date = new Date(created_date);
+      access_date.setMinutes(access_date.getMinutes() + 15);
+      const refresh_date = new Date(created_date);
+      refresh_date.setDate(refresh_date.getDate() + 30);
+
+      if (now > access_date) {
+        if (now < refresh_date) {
+          console.log("Refresh token", auth_data)
+          const resp = await refreshAuthToken(auth_data.refresh);
+          if (resp.error) {
+            err_msg = "Failed to refresh access token."
+            if (resp.details) {
+              err_msg += ` Error: ${resp.details.detail}`;
+            } else if (resp.errmsg) {
+              err_msg += ` Error(${resp.errcode}): ${resp.errmsg}`;
+            }
+          } else if (resp.data) {
+            auth_data = resp.data
+          }
+        } else {
+          console.log("Tokens are invalid");
+          // Existing tokens are invalid
+          auth_data = undefined;
+        }
+      }
+    }
+
+    if (err_msg) {
+      console.error(err_msg);
+      console.info("Will try to authenticate with username and password");
+    }
+
+    if (!auth_data) {
+      console.log("Authenticate with username and password")
+      const user = await getLocalUser();
+      console.log("user", user)
+      if (user) {
+        const resp = await authUser(user);
         if (resp.error) {
-          err_msg = "Failed to refresh access token."
+          err_msg = "Failed to authenticate user."
           if (resp.details) {
-            err_msg += ` Error: ${resp.details.detail}`;
-          } else if (resp.errmsg) {
-            err_msg += ` Error(${resp.errcode}): ${resp.errmsg}`;
+            err_msg += " Error: ";
+            if (resp.details.detail) {
+              err_msg += resp.details.detail;
+            } else {
+              if (resp.details.username) {
+                err_msg += "'username' field may not be blank"
+              } else if (resp.details.password) {
+                if (resp.details.username) { err_msg += " and "; }
+                err_msg += "'password' field may not be blank"
+              }
+            }
           }
         } else if (resp.data) {
           auth_data = resp.data
         }
       } else {
-        console.log("Tokens are invalid");
-        // Existing tokens are invalid
-        auth_data = undefined;
+        err_msg = "Need to provide username and password"
       }
     }
-  }
 
-  if (err_msg) {
-    console.error(err_msg);
-    console.info("Will try to authenticate with username and password");
-  }
-
-  if (!auth_data) {
-    console.log("Authenticate with username and password")
-    const user = await getLocalUser();
-    console.log("user", user)
-    if (user) {
-      const resp = await authUser(user);
-      if (resp.error) {
-        err_msg = "Failed to authenticate user."
-        if (resp.details) {
-          err_msg += " Error: ";
-          if (resp.details.detail) {
-            err_msg += resp.details.detail;
-          } else {
-            if (resp.details.username) {
-              err_msg += "'username' field may not be blank"
-            } else if (resp.details.password) {
-              if (resp.details.username) { err_msg += " and "; }
-              err_msg += "'password' field may not be blank"
-            }
-          }
-        }
-      } else if (resp.data) {
-        auth_data = resp.data
-      }
-    } else {
-      err_msg = "Need to provide username and password"
+    if (err_msg) {
+      console.error(err_msg)
+    } else if (auth_data === undefined) {
+      console.error(`Have exhausted all options to authenticate you. Only advice would be to make sure that account credentials are right and keep trying from time to time.`);
     }
+    return auth_data;
   }
 
-  if (err_msg) {
-    console.error(err_msg)
-  } else if (auth_data === undefined) {
-    console.error(`Have exhausted all options to authenticate you. Only advice would be to make sure that account credentials are right and keep trying from time to time.`);
-  }
-  return auth_data;
-}
-
-function isAccessTokenValid(auth_data: AuthDataTime) {
-    const now = new Date();
-    const created_date = new Date(auth_data.created_at);
-    const access_date = new Date(created_date);
-    access_date.setMinutes(access_date.getMinutes() + 15);
-    return now < access_date;
-}
-
-async function init() {
-  let auth_data = await getLocalAuthData();
-  // let auth_data;
-
-  if (auth_data === undefined || !isAccessTokenValid(auth_data)) {
+  async function newToken(auth_data: AuthDataTime | undefined): Promise<AuthDataTime | undefined> {
     let new_token = await requestNewToken(auth_data);
     if (new_token) {
-      // save new auth_data
-      auth_data = await setLocalAuthData(new_token);
+      auth_data = { created_at: Date.now(), token: new_token };
+    }
+
+    return auth_data;
+  }
+
+  function isAccessTokenValid(created_at: number) {
+      const now = new Date();
+      const created_date = new Date(created_at);
+      const access_date = new Date(created_date);
+      access_date.setMinutes(access_date.getMinutes() + 15);
+      return now < access_date;
+  }
+
+  return {
+    newToken: newToken,
+    isAccessTokenValid: isAccessTokenValid,
+  };
+})();
+
+async function init() {
+  let curr_auth_data = await getLocalAuthData();
+  let new_auth_data = curr_auth_data;
+  if (curr_auth_data === undefined || !histre.isAccessTokenValid(curr_auth_data.created_at)) {
+    // Get new token
+    const auth_data = await histre.newToken(curr_auth_data);
+    if (auth_data) {
+      await setLocalAuthData(auth_data);
+      new_auth_data = auth_data;
     }
   }
-  console.log("init", auth_data)
 
+  if (new_auth_data === undefined) {
+    console.error("Failed to get valid Histre user token");
+    return;
+  }
+
+  console.log(new_auth_data);
 }
-init()
+
+init();
 
 // browser.runtime.openOptionsPage()
 // const bg_href = (await browser.runtime.getBackgroundPage()).location.href;

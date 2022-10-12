c483a3aa3f6dbc9ebf7ff7a0a9670c47a45747ed
last-arg <last_arg@tutanota.com>
2022-10-12 19:11:45 +0300

    get highlights close to removed highlight

--- src/content_script.ts
+++ src/content_script.ts
@@ -533,6 +533,95 @@ async function removeHighlight(id: string, url: string) {
   removeHighlightFromDom(highlights, elems);
 }
 
+function removeHighlightFromDom(highlights: LocalHighlightsObject, elems: NodeListOf<Element>) {
+  // TODO: have to hold on to histre highlights and local highlights.
+  // Or get local highlight when needed?
+  console.log("elems to remove: ", elems.length)
+  console.log("elems length: ", elems[0].textContent!.length)
+  for (const fill_elem of elems) {
+    let prev = fill_elem.previousSibling;
+    const prev_elems: [Element, number][] = [];
+    let total_len = 0;
+    while (prev) {
+      if (prev.nodeType === 3) {
+        const text_len = prev.textContent?.length || 0;
+        if (text_len === 0) {
+          prev = prev.previousSibling;
+          continue;
+        } else if (text_len > 0) {
+          break;
+        }
+      }
+      if (prev.nodeType === 1 && !(prev as Element).classList.contains("hho-mark")) {
+        break;
+      }
+
+      total_len += prev.textContent?.length || 0;
+      prev_elems.push([prev as Element, total_len]);
+      prev = prev.previousSibling;
+    }
+
+    if (prev_elems.length === 0) {
+      console.log("ALONE")
+      fill_elem.replaceWith(fill_elem.textContent!);
+      continue;
+    }
+
+    console.log("prev", prev_elems);
+    console.log("prev", prev_elems.length);
+    const filtered_elems = prev_elems.filter(([elem, _], elem_index, arr) => {
+      const id = elem.getAttribute("data-hho-id");
+      const index = arr.findLastIndex(([el, _]) => el.getAttribute("data-hho-id") === id);
+      return elem_index === index;
+    })
+    console.log("filter", filtered_elems.length)
+    let fill_len = fill_elem.textContent?.length || 0;
+    let fill_text_node: Node | undefined = undefined;
+    for (const [f_elem, len] of filtered_elems) {
+      const curr_id = f_elem.getAttribute("data-hho-id")!;
+      const {text, color} = highlights[curr_id];
+      // TODO: make sure f_elem is start of highlight?  
+      const total_len = len + fill_len;
+      if (text.length >= total_len && fill_text_node === undefined) {
+        console.log("REPLACE")
+        fill_elem.setAttribute("data-hho-id", curr_id);
+        fill_elem.setAttribute("data-hho-color", color!);
+        break;
+      } else if (text.length > len) {
+        console.log("MULTIPLE")
+        // Highlight only covers part of 'removed' node
+        // TODO: highlight only covers part of node
+        // remove surrounding <mark>
+        // split text node in right place
+        // change text node 
+        if (fill_text_node === undefined) {
+          fill_text_node = fill_elem.firstChild!;
+          // fill_elem.parentNode?.insertBefore(fill_text_node, fill_elem);
+          // fill_elem.remove();
+
+          fill_elem.replaceWith(fill_text_node);
+          const text_end = (fill_text_node as Text).splitText(text.length - len);
+
+          const r = document.createRange();
+          r.selectNode(fill_text_node);
+          const new_mark = mark_elem.cloneNode(true) as Element;
+          new_mark.setAttribute("data-hho-id", curr_id);
+          new_mark.setAttribute("data-hho-color", color!);
+          r.surroundContents(new_mark)
+          fill_text_node = text_end;
+        }
+      } else if (fill_text_node === undefined) {
+        console.log("JUST remove")
+        fill_elem.replaceWith(fill_elem.textContent!);
+        break;
+      }
+
+      // console.log(text)
+      // console.log(text.slice(0,len))
+    }
+  }
+}
+
 function init() {
   const url = window.location.href;
   if(document.readyState === 'loading') {
@@ -586,13 +675,24 @@ async function test() {
       highlightDOM(indices, current_entries)
 
       // Tests
+      // {
+      //   const before_elems = document.querySelectorAll(`[data-hho-id="2"]`);
+      //   console_expect(1, before_elems.length)
+      //   console_expect(2, document.querySelectorAll(`[data-hho-id="1"]`).length);
+      //   removeHighlightFromDom(test_highlights, before_elems)
+      //   console_expect(0, document.querySelectorAll(`[data-hho-id="2"]`).length);
+      //   console_expect(3, document.querySelectorAll(`[data-hho-id="1"]`).length);
+      // }
+
       {
-        const before_elems = document.querySelectorAll(`[data-hho-id="2"]`);
+        const before_elems = document.querySelectorAll(`[data-hho-id="5"]`);
         console_expect(1, before_elems.length)
-        console_expect(2, document.querySelectorAll(`[data-hho-id="1"]`).length);
+        console_expect(2, document.querySelectorAll(`[data-hho-id="3"]`).length);
+        console_expect(1, document.querySelectorAll(`[data-hho-id="4"]`).length);
         removeHighlightFromDom(test_highlights, before_elems)
-        console_expect(0, document.querySelectorAll(`[data-hho-id="2"]`).length);
-        console_expect(3, document.querySelectorAll(`[data-hho-id="1"]`).length);
+        console_expect(0, document.querySelectorAll(`[data-hho-id="5"]`).length)
+        console_expect(3, document.querySelectorAll(`[data-hho-id="3"]`).length);
+        console_expect(2, document.querySelectorAll(`[data-hho-id="4"]`).length);
       }
   }
 }
